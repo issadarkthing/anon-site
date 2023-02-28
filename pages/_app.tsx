6ac63077581de04b858f3f30bf2a9f8716c4e0c4
raziman <raziman@tutanota.com>
2023-03-01 01:35:15 +0800

    fix inconsistent color on chrome browser

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider, createTheme } from "@mui/material/styles"
 
 const queryClient = new QueryClient();
 
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      color: "whitesmoke",
+    }
+  }
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThemeProvider>
   )
 }
