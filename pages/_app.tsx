8594bca0951a116f5bd0ed22823cf5f2f4455707
raziman <raziman@tutanota.com>
2023-03-01 13:35:08 +0800

    code refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider, createTheme } from "@mui/material/styles"
+import styles from "@/styles/Home.module.css";
+import Box from '@mui/material/Box';
 
 const queryClient = new QueryClient();
 
@@ -17,7 +19,29 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <main className={styles.main}>
+          <Box
+            display="flex" 
+            flexDirection="column" 
+            gap="20px"
+            sx={(theme) => ({
+              [theme.breakpoints.up("lg")]: {
+                width: "40vw",
+              },
+              [theme.breakpoints.up("md")]: {
+                width: "50vw",
+              },
+              [theme.breakpoints.up("sm")]: {
+                width: "80vw",
+              },
+              [theme.breakpoints.down("sm")]: {
+                width: "90vw",
+              },
+            })}
+          >
+            <Component {...pageProps} />
+          </Box>
+        </main>
       </QueryClientProvider>
     </ThemeProvider>
   )
