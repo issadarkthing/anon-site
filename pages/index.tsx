b8666e7c433214e79325868fc241cec7fee04d8b
raziman <raziman@tutanota.com>
2023-03-06 16:36:49 +0800

    improve index page
    improve index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Typography from '@mui/material/Typography';
 import Head from "next/head";
 import { Header } from "@/components/Header";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
@@ -10,10 +12,29 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Header href="/login" />
-      <Typography variant="subtitle1">
-        Anonymous messaging 
-        created by <a style={{ color: "lightblue" }} href="https://issadarkthing.com">issadarkthing</a>
-      </Typography>
+      <Box 
+        display="flex" 
+        flexDirection="column" 
+        alignItems="center"
+        justifyContent="center"
+        gap="90px"
+      >
+        <Typography variant="h5">
+          Send a message to the person you want anonymously.
+        </Typography>
+        <Box
+          display="flex"
+          gap="30px"
+          sx={{ width: "100%" }}
+        >
+          <Button fullWidth variant="contained" href="/login">
+            Log In
+          </Button>
+          <Button fullWidth variant="contained" href="/signup">
+            Sign Up
+          </Button>
+        </Box>
+      </Box>
     </>
   )
 }
