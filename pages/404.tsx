0449d9f92bf2edb9322a5c43c6e23325f336cc81
raziman <raziman@tutanota.com>
2023-03-06 16:00:35 +0800

    improve 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Typography from "@mui/material/Typography";
+import { Header } from "@/components/Header";
+import { Box } from "@mui/material";
 
 
 export default function Page404() {
@@ -10,9 +12,28 @@ export default function Page404() {
         <title>anonmi</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Typography variant="h4">
-        404 | Page Not Found
-      </Typography>
+      <Box 
+        display="flex" 
+        flexDirection="column" 
+        justifyContent="center"
+        gap="30px"
+      >
+        <Header href="/" />
+        <Box display="flex" justifyContent="center">
+          <Typography variant="h4">
+            404 | Page Not Found
+          </Typography>
+        </Box>
+        <Typography variant="h6" sx={{ fontWeight: "light" }}>
+          The page you are looking for is not available, please make sure the
+          url that you copied is correct and does not contain any invalid
+          characters.
+          <br />
+          <br />
+          If you are 100% sure the url is correct then it might be due to 
+          account deletion or a permanent ban.
+        </Typography>
+      </Box>
     </>
   )
 }
