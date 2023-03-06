05ddb77a91d847fbfd1c083e903e15ba4ed075b0
raziman <raziman@tutanota.com>
2023-03-06 16:36:44 +0800

    add accent color to logo name

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,8 +8,8 @@ export function Header(props: { href?: string }) {
   const main = (
     <Box display="flex" justifyContent="center" alignItems="center" gap="7px">
       <img src="/anon.svg" alt="logo" style={{ width: "30px" }} /> <br />
-      <Typography variant="h3" sx={{ fontFamily: "Comic Cat" }}>
-        anonmi
+      <Typography display="inline" variant="h3" sx={{ fontFamily: "Comic Cat" }}>
+        anon<span style={{ color: "#208dd4" }}>mi</span>
       </Typography>
     </Box>
   );
