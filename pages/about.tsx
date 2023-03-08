e4ad2428dbf630f7e328db7f8da7222f8dfe2ab5
raziman <raziman@tutanota.com>
2023-03-08 12:57:12 +0800

    change text

--- pages/about.tsx
+++ pages/about.tsx
@@ -21,10 +21,10 @@ export default function About() {
         <Header href="/" />
         <Typography variant="body1">
           <Name variant="h6" display="inline"/> is an anonymous Q&A platform.
-          The platform should give the freedom to users to send you a message
-          without having to disclose their personal info. No account creation
-          is needed when sending a message to any user which makes it easier 
-          for anyone to use this application.
+          The platform should give the freedom to users to send message without
+          having to disclose their personal info. No account creation is needed
+          when sending a message to any user which makes it easier for anyone to
+          use this application.
         </Typography>
       </Box>
     </>
